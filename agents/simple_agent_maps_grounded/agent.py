453eacb7fb31b2d60c46d65d9547f94ea6f25a93
Folken2 <folkenai21@gmail.com>
2025-12-23 13:16:57 +0100

    fix google search and google maps agents to use google ai studio

--- agents/simple_agent_maps_grounded/agent.py
+++ agents/simple_agent_maps_grounded/agent.py
@@ -6,9 +6,6 @@ Simple Agent with Web Search Capabilities
 from google.adk.agents import LlmAgent
 from google.adk.tools import google_search, google_maps_grounding
 
-## config imports
-from .config.llm import FAST_MODEL, REASONING_MODEL
-
 ## prompt imports
 from .prompt.prompt import prompt_v0
 
@@ -16,7 +13,7 @@ from .prompt.prompt import prompt_v0
 
 root_agent = LlmAgent(
     name="google_maps_search_agent",
-    model=FAST_MODEL,
+    model="gemini-2.5-flash",
     description="AI assistant that grounds answers using google maps search and always cites sources",
     tools=[google_maps_grounding],
     instruction=prompt_v0,
--- agents/simple_agent_web_search/agent.py
+++ agents/simple_agent_web_search/agent.py
@@ -6,9 +6,6 @@ Simple Agent with Web Search Capabilities
 from google.adk.agents import LlmAgent
 from google.adk.tools import google_search
 
-## config imports
-from .config.llm import FAST_MODEL, REASONING_MODEL
-
 ## prompt imports
 from .prompt.prompt import prompt_v0
 
@@ -16,7 +13,7 @@ from .prompt.prompt import prompt_v0
 
 root_agent = LlmAgent(
     name="web_search_agent",
-    model=FAST_MODEL,
+    model="gemini-2.5-flash",
     description="AI assistant that grounds answers using web search and always cites sources",
     tools=[google_search],
     instruction=prompt_v0,
